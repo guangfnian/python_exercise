b400d73588b592953406ca2df8e3725f7ce81074
Gazali Dai <yuujinnx@gmail.com>
2016-02-27 10:42:22 +0800

    add NO.0000

---
+++ 0000.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from PIL import Image, ImageDraw, ImageFont
+im = Image.open(input('Please drag the picture here...\n'))
+num = int(input('Please input the number...\n'))
+w, h = im.size
+font = ImageFont.truetype('c:/Windows/Fonts/Arial.ttf',36)
+draw = ImageDraw.Draw(im)
+if num <= 0:
+	print('Error! The number should be larger than 0')
+elif num < 10:
+	draw.text((w-25, 5), str(num), font = font, fill = (255,0,0))
+elif num < 100:
+	draw.text((w-50, 5), str(num), font = font, fill = (255,0,0))
+else:
+	draw.text((w-75, 5), '99+', font = font, fill = (255,0,0))
+im.save('hhh.jpg', 'jpeg')
