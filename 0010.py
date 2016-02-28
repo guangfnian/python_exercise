213b394a995101fc1fd5ba01a6303d9546c510d4
Gazali Dai <yuujinnx@gmail.com>
2016-02-28 09:34:37 +0800

    add No.0010

---
+++ 0010.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
+import random
+
+def rand_color():
+	return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+
+def rand_chr():
+	return chr(random.randint(65, 90))
+
+w, h, num = 200, 100, 4
+im = Image.new('RGB', (w, h), (255, 255, 255))
+draw = ImageDraw.Draw(im)
+font = ImageFont.truetype('c:/Windows/Fonts/Arial.ttf', 50)
+for x in range(w):
+	for y in range(h):
+		draw.point((x, y), fill = rand_color())
+for i in range(num):
+	draw.text((i*50, 20), rand_chr(), font = font, fill = rand_color())
+im = im.filter(ImageFilter.BLUR)
+im.save("code.jpg", 'jpeg')
